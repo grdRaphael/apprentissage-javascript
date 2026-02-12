e7ae2e1dea58b6d09a19ea71fd837b6a2add8aa6
grdRaphael <drebin235@gmail.com>
2026-02-12 17:47:04 +0400

    exercise complete : organiser le code grace aux fonctions

---
+++ Premier projet javascript/config.js
@@ -0,0 +1,3 @@
+// Listes de mots et de phrase
+let listeMots = ['a', 'b', 'c']
+let listePhrases = ['La vie', "Un univers", 'Le monde']
--- Premier projet javascript/script.js
+++ Premier projet javascript/script.js
@@ -1,5 +1,5 @@
 
-function exercice1() {
+function executeExo1() {
     /* =================================================================================
     Étape 1 : Testez le premier mot
     Déclarez une variable listeMots qui est un tableau. 
@@ -12,7 +12,7 @@ function exercice1() {
 
     let score = 0
 
-    /* À l’aide de l’instruction prompt, demandez à l’utilisateur de rentrer le mot contenu dans la première case du tableau. 
+    /* À l’aide de l’instruction prompt, demandezà l’utilisateur de rentrer le mot contenu dans la première case du tableau. 
     N’oubliez pas, la première case d’un tableau est la case numéro zéro !*/
 
     let wordUser = prompt('Entrez le mot : ' + listeMots[0])
@@ -62,7 +62,7 @@ function exercice1() {
 
 
 
-function exercice2() {
+function executeExo2() {
 
     /* Étape 1 : répétez votre code avec une boucle
     Demandez à l’utilisateur de le retaper avec prompt ;
@@ -80,44 +80,106 @@ function exercice2() {
 
     let listeMots = ['a', 'b', 'c']
     let listePhrases = ['La vie', "Un univers", 'Le monde']
-
-
     let score = 0
 
 
     let choixExercices = prompt("Choisissez votre exercice Tapez : phrases ou mots")
 
     while (choixExercices !== 'phrases' && choixExercices !== 'mots') {
-    choixExercices = prompt("Choisissez votre exercice Tapez : phrases ou mots")
+        choixExercices = prompt("Choisissez votre exercice Tapez : phrases ou mots")
     }
 
-    if (choixExercices === "phrases") {
-        exercicePhrases()
-    } else{
-        exerciceMots()
-    } 
-
-    function exercicePhrases() {
+    if (choixExercices === 'phrases') {
         for (i = 0; i < listePhrases.length; i++) {
             let sentenceUser = prompt('Entrez le mot : ' + listePhrases[i])
             if (sentenceUser === listePhrases[i]) {
                 score++
             }
-            
+
         }
         console.log("Bravo ! Votre score est de : " + score + " sur " + listePhrases.length)
-    }
-
-    function exerciceMots() {
+    } else {
         for (i = 0; i < listeMots.length; i++) {
             let wordUser = prompt('Entrez le mot : ' + listeMots[i])
             if (wordUser === listeMots[i]) {
                 score++
             }
-            
+
         }
-        console.log("Bravo ! Votre score est de : " + score + ' sur ' + listeMots.length)
+        console.log("Bravo ! Votre score est de : " + score + " sur " + listePhrases.length)
     }
+
 }
 
-exercice2()
+
+
+/* Étape 1 : découpez votre code en fonctions
+
+Notre but est maintenant d’améliorer l’organisation et la lisibilité de notre 
+code. Je vous propose donc de créer les fonctions suivantes : 
+
+afficherResultat : cette fonction prend en paramètre le résultat et 
+le nombre total de mots proposés, et affiche le résultat du joueur ; 
+
+choisirPhrasesOuMots : cette fonction demande à l’utilisateur s’il veut jouer 
+avec des phrases ou des mots. 
+
+lancerBoucleDeJeu : cette fonction contient la boucle principale de jeu, c'est-à-dire la boucle for 
+qui propose les mots/phrases au joueur, et lui demande de taper ces mots. Elle prend en paramètre
+le tableau de mots/phrases qui sera proposé au joueur, et retourne le nombre de mots/phrases 
+correctement tapés ;
+
+lancerJeu : cette fonction sera la fonction principale, c’est elle qui s’occupe de lancer 
+toutes les autres. En d’autres termes, c’est elle qui va appeler les fonctions que vous venez
+d’écrire.
+*/
+
+
+
+function executeExo3() {
+
+    let score = 0
+
+    function choisirPhrasesOuMots() {
+        let choixExercices = prompt("Choisissez votre exercice Tapez : phrases ou mots")
+
+        while (choixExercices !== 'phrases' && choixExercices !== 'mots') {
+            choixExercices = prompt("Choisissez votre exercice Tapez : phrases ou mots")
+        }
+        return choixExercices
+    }
+
+    function lancerBoucleDeJeu(liste) {
+
+        for (i = 0; i < liste.length; i++) {
+            let sentenceUser = prompt('Entrez le mot : ' + liste[i])
+            if (sentenceUser === liste[i]) {
+                score++
+            }
+        }
+        return score
+    }
+
+    function lancerJeu() {
+        let choix = choisirPhrasesOuMots();
+        let nbMotPropose = 0;
+        if (choix === "mots") {
+            lancerBoucleDeJeu(listeMots);
+            nbMotPropose = listeMots.length
+        } else {
+            lancerBoucleDeJeu(listePhrases);
+            nbMotPropose = listePhrases.length
+        }
+        afficherResultat(score, nbMotPropose);
+    }
+
+    function afficherResultat(score, nbMotPropose) {
+        console.log("Bravo ! Votre score est de : " + score + " sur " + nbMotPropose)
+    }
+
+    lancerJeu()
+
+}
+
+executeExo3()
+
